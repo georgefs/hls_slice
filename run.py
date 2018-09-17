3ec2d6ba5b6103e08bb81963c4fdfef0a136477c
georgeli <george.li@gliacloud.com>
2018-09-17 17:14:46 +0800

    add list page

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, request, redirect, static_file, default_app
+from bottle import route, run, template, request, redirect, static_file, default_app, response, hook
 import subprocess
 import tempfile
 import os
@@ -14,8 +14,16 @@ global pool
 streaming_downloader = None
 pool = {}
 
+slices = {}
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+
+hook('after_request')
+def set_unicode():
+    response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+
 
 def to_static_url(path):
     global BASE_DIR
@@ -36,7 +44,7 @@ def start():
     m3u8_url = request.forms.get('m3u8_url')
     temp_dir = tempfile.mkdtemp(dir=STATIC_PATH)
     m3u8_path = os.path.join(temp_dir, 'index.m3u8')
-    cmd = 'ffmpeg -loglevel panic -i {} -start_number 0 -hls_time 2 -hls_list_size 0 -g 1 -vcodec copy -f hls {}'.format(m3u8_url, m3u8_path).split()
+    cmd = 'ffmpeg -loglevel panic -i {} -start_number 0 -hls_time 1 -hls_list_size 0 -g 1 -vcodec copy -f hls {}'.format(m3u8_url, m3u8_path).split()
     streaming_downloader = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     time.sleep(5)
     return redirect('main')
@@ -51,6 +59,7 @@ def slice():
     global pool
     start = int(request.forms.get('start'))
     end = int(request.forms.get('end'))
+    msg = request.forms.msg
 
     m3u8 = hls.M3U8.from_path(m3u8_path).slice_idx(start, end)
     name = os.path.join(temp_dir, '{}_{}'.format(start, end))
@@ -66,7 +75,21 @@ def slice():
     info['proc'] = proc
 
     pool[proc.pid] = info
+
+    slices[mp4_dir] = {"msg": msg, "start": start, "end": end, "url": to_static_url(mp4_dir), "status": None}
     return redirect('/wait/{}'.format(proc.pid))
+
+@route('/list')
+def list():
+    html = "<meta charset='UTF-8'>"
+    html += "<table>"
+    html += "<tr><td>start</td><td>end</td><td>msg</td><td>status</td></tr>"
+
+    tr = "<tr><td>{start}</td><td>{end}</td><td><a href={url}>{msg}</a></td><td>{status}</td></tr>"
+    for name, info in slices.items():
+        html += tr.format(**info)
+    html += "</table>"
+    return html
 
 @route('/wait/<pid:re:\d+>')
 def wait(pid):
@@ -74,6 +97,7 @@ def wait(pid):
     info = pool[int(pid)]
     # success
     code = info['proc'].poll()
+    slices[info['mp4']]['status'] = code
     if code == 0:
         return redirect(to_static_url(info['mp4']))
     else:
